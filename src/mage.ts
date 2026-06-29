652d4ee0b17faf129aeb7bf39d0787b278f2084e
tengzelaitong-web <tengzelaitong0@gmail.com>
2026-06-29 22:56:42 +0900

    １

--- src/mage.ts
+++ src/mage.ts
@@ -1,7 +1,12 @@
-import { Character } from './character.ts';
+import { Character } from "./character";
 
 export class Mage extends Character {
-  override attack() {
-    console.log(`${this.name}は魔法を唱えた！`);
+  constructor(name: string, hp: number) {
+    super(name, hp);
+  }
+
+  override attack(opponent: Character): void {
+    console.log(`${this.name}は魔法を放った！`);
+    opponent.takeDamage(15);
   }
 }
--- src/warrior.ts
+++ src/warrior.ts
@@ -1,14 +1,15 @@
-import { Character } from './character.ts';
+import { Character } from "./character.ts";
 
 export class Warrior extends Character {
   private weapon: string;
 
   constructor(name: string, hp: number, weapon: string) {
-    super(name, hp); // super() <- 継承元 (character) の constructor
+    super(name, hp);
     this.weapon = weapon;
-  };
+  }
 
-  override attack() {
+  override attack(opponent: Character): void {
     console.log(`${this.name}は${this.weapon}で攻撃した！`);
-  };
+    opponent.takeDamage(10);
+  }
 }
